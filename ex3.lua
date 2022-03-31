8c0dd1686de65772ca8fd91c11fe5286837f738e
Jedid95 <jedid.santos@gmail.com>
2022-04-01 00:25:35 +0100

    expressões

---
+++ ex3.lua
@@ -0,0 +1,34 @@
+a = 10
+b = 22
+print(a+b)
+print(a-b)
+print(-b)
+print(a/b)
+print(b/a)
+print(b^2)
+print(b^0.5)
+
+c = b^0.5
+print(c)
+print(c*c)
+
+print(a > b)
+print(a < b)
+print(a<= b)
+print(a <= 10)
+print("a" == "a")
+print("a" == "b")
+print("abacate" < "abacaxi")
+print({} == {})
+tab1 = {}
+tab2 = tab1
+print(tab1 == tab2)
+
+a = true
+b = false
+print(a and b)
+b = true
+print(a and b)
+print(a or b)
+b = false
+print(a or b)
